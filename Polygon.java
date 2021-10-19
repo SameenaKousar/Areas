ae5bfa9fa97f8c8d5ebb0e1e9583c1bbbd6818d2
Sameena Kousar <92714138+SameenaKousar@users.noreply.github.com>
2021-10-19 17:35:14 +0530

    first commit

---
+++ Polygon.java
@@ -0,0 +1,44 @@
+package project1;
+
+public interface Polygon {
+	double pi=3.14;
+	 public double circle(double x);
+	int rectangle(int x, int y);
+	double triangle(double a, double b);
+	int square(int s);
+	double hexagon(double b);
+	public static void main(String[] args) {
+		Areas a1= new Areas();
+		a1.circle(10);
+		a1.rectangle(10,20);
+		a1.triangle(3,5);
+		a1.square(4);
+		a1.hexagon(10);
+		
+	}
+}
+class Areas implements Polygon{
+	public double circle(double a) {
+		System.out.println("area of circle:" +pi*a*a);
+		return(pi*a*a);
+	}
+	public int rectangle(int x, int y) {
+		System.out.println("rectangle area:"+x*y);
+		return(x*y);		
+	}
+	public double triangle(double a, double b) {
+		System.out.println("area of triangle:"+a*b/2);
+		return(a*b/2);
+		
+	}
+	public int square(int s) {
+		System.out.println("area of square:" +s*s);
+		return(s*s);
+		
+	}
+	public double hexagon(double b) {
+		double area=((3*Math.pow(3, 1/3)*Math.pow(b, 2)))/2;
+		System.out.println("area of hexagon:" +area);
+		return area;
+	}
+}
